19bd16fa88abde749ca93395a92701aa67355003
Brandon Hadler <brandon.hadler30@gmail.com>
2018-09-24 21:16:03 -0700

    add whileExponent and recursionExponent functions

--- recursion/recursionIntro.js
+++ recursion/recursionIntro.js
@@ -21,8 +21,26 @@ function recursionLoop(n) {
 }
 
 //3.Write a function 'exponent' that takes two arguments base, and expo, uses a while loop to return the exponenet value of the base.
+function whileExponent(base, expo) {
+  var result = base;
+
+  while (expo !== 1) {
+    result = result * base;
+
+    expo--;
+  }
+
+  return result;
+}
 
 //4. Write a function 'RecursiveExponent' that takes two arguments base, and expo, recursively returns exponent value of the base.
+function recursionExponent(base, expo) {
+  if (expo === 1) {
+    return base;
+  } else {
+    return base * recursionExponent(base, expo - 1);
+  }
+}
 
 //5. Write a function 'recursiveMultiplier' that takes two arguments, 'arr and num', and multiplies each arr value into by num and returns an array of the values.
 
